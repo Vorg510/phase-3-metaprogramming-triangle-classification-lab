0e0e5391bae99b02aef1dac09589adb8266324ab
Yuferril <yuferril.yunos@gmail.com>
2023-01-31 20:02:50 +1100

    commit tree 7742f0b68d27e66539e5d87e1a6396ccf387c110

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || c == a
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  def no_size
+    if ([a, b, c] || 0) == 0
+        raise TriangleError
+        p message
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "LLaaaaaa"
+    end
+  end
 end
+
